831ff7a251ba1db301262e37ead426199edc278d
Andrey <firebird_07@mail.ru>
2020-06-16 12:02:41 +0300

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ class Form extends Component {
     render() {
         return(
             <form onSubmit={this.props.getWeather}>
-                <input type="text" name="city" placeholder="City..."/>
+                <input type="text" name="city" placeholder="City..." required/>
                 <input type="text" name="country" placeholder="Country code..."/>
                 <button type="submit">Get Weather</button>
             </form>

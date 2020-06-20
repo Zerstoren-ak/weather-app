c4cdb71019dbbe9a4dd5046f61430b59f0b31301
Andrey <firebird_07@mail.ru>
2020-06-20 14:47:05 +0300

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 
-class Form extends Component {
-    render() {
-        return(
-            <form onSubmit={this.props.getWeather}>
-                <input type="text" name="city" placeholder="City..." required/>
-                <input type="text" name="country" placeholder="Country code..."/>
-                <button type="submit">Get Weather</button>
-            </form>
-        )
-    }
+function Form(props) {
+    return (
+        <form onSubmit={props.getWeather}>
+            <input type="text" name="city" placeholder="City..." required/>
+            <input type="text" name="country" placeholder="Country code..."/>
+            <button type="submit">Get Weather</button>
+        </form>
+    )
 }
 
 export default Form

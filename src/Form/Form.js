24a9df2dcf38c4d1470a956a5d1e8ff5a43fff55
Andrey <firebird_07@mail.ru>
2020-06-23 23:11:41 +0300

    wrapper, no success yet

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Form(props) {
     return (
-        <form onSubmit={props.addCity}>
+        <form onSubmit={props.addCity} >
             <input type="text" name="city" placeholder="City..." required/>
             <input type="text" name="country" placeholder="Country code..."/>
             <button type="submit">Get Weather</button>

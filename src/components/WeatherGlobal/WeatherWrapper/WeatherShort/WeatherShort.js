cd5c674e034797dd85381bbe0e5f078926ebd802
Andrey <firebird_07@mail.ru>
2020-07-04 22:39:44 +0300

    flawless victory

--- src/components/WeatherGlobal/WeatherWrapper/WeatherShort/WeatherShort.js
+++ src/components/WeatherGlobal/WeatherWrapper/WeatherShort/WeatherShort.js
@@ -5,7 +5,7 @@ import "./WeatherShort.css"
 function WeatherShort(props) {
     console.log('WeatherShort received props', props.city);
     return (
-        <div className={'WeatherShort'} onClick={props.clickHandlerExpand}>
+        <div className={'WeatherShort'}>
             <div>
                 <Clock/>
                 <div className={'location'}>
@@ -14,7 +14,8 @@ function WeatherShort(props) {
                 </div>
             </div>
             {!props.weather ? <h3>t&deg;C</h3>  : <h3>{Math.round(props.weather.main.temp) + '\u00B0C'}</h3>  }
-            <button onClick={props.clickHandlerRemove}>X</button>
+            <button className={'weather-remove'} onClick={props.clickHandlerRemove}>X</button>
+            <button className={'weather-expand'} onClick={props.clickHandlerExpand}>expand</button>
         </div>
     )
 }

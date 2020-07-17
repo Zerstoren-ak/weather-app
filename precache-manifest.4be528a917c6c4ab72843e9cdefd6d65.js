7dbc5ca7fbc4be71891545deab7a5dca4abc899a
Andrey <firebird_07@mail.ru>
2020-07-17 19:15:18 +0300

    Updates

---
+++ precache-manifest.4be528a917c6c4ab72843e9cdefd6d65.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15afca406ef29e7780419319bac93a9",
+    "url": "/weather-app-deploy/index.html"
+  },
+  {
+    "revision": "8a8bcc57bcd7988b34ec",
+    "url": "/weather-app-deploy/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "0e82ec5278ee22cab239",
+    "url": "/weather-app-deploy/static/css/main.57b1d93a.chunk.css"
+  },
+  {
+    "revision": "8a8bcc57bcd7988b34ec",
+    "url": "/weather-app-deploy/static/js/2.389e9992.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/weather-app-deploy/static/js/2.389e9992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e82ec5278ee22cab239",
+    "url": "/weather-app-deploy/static/js/main.ee2b360a.chunk.js"
+  },
+  {
+    "revision": "b2894574be711163ddf4",
+    "url": "/weather-app-deploy/static/js/runtime-main.9a76cf67.js"
+  },
+  {
+    "revision": "cc38b2a29266c3fe857faf81b567c930",
+    "url": "/weather-app-deploy/static/media/Clouds-day.cc38b2a2.mp4"
+  },
+  {
+    "revision": "6f2e858e037e7b51c3f730439d6f2c5d",
+    "url": "/weather-app-deploy/static/media/Drizzle-day.6f2e858e.mp4"
+  },
+  {
+    "revision": "62f5a769f66fec839b8a62e2251294c4",
+    "url": "/weather-app-deploy/static/media/Fog-day.62f5a769.mp4"
+  },
+  {
+    "revision": "de58e629b12f49d374ef6a467a7be228",
+    "url": "/weather-app-deploy/static/media/Rain-day.de58e629.mp4"
+  },
+  {
+    "revision": "bf29e32114146ea337e7c5015e307440",
+    "url": "/weather-app-deploy/static/media/Snowfall.bf29e321.mp4"
+  },
+  {
+    "revision": "ae0becdc8a00d1845125e18b47cec50f",
+    "url": "/weather-app-deploy/static/media/Sun.ae0becdc.mp4"
+  },
+  {
+    "revision": "b0b97a7c954a6e2af0394139926511b3",
+    "url": "/weather-app-deploy/static/media/Thunderstorm-day.b0b97a7c.mp4"
+  },
+  {
+    "revision": "0411f2577222cbc74770da0f89b38b48",
+    "url": "/weather-app-deploy/static/media/main-bg.0411f257.jpeg"
+  }
+]);
